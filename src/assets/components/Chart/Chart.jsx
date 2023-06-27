15b2eeff5b551176bfdf16d1d1d195a1698558c1
Da-Hubb <josemariaofurum@gmail.com>
2023-06-27 06:15:39 +0100

    max spent

--- src/assets/components/Chart/Chart.jsx
+++ src/assets/components/Chart/Chart.jsx
@@ -3,12 +3,13 @@ import data from './../../../assets/data/data.json';
 import { useGlobalContext } from '../../globalHook/globalHook';
 
 // component bar
-function Bar({day, amount, show, handleHover, handleHoverOut}) {
+function Bar({day, amount, show, handleHover, handleHoverOut, maximus}) {
     return <div className="bar text-center d-flex flex-column justify-content-end">
         {show && <span className="price w-100 badge rounded-pill">
             {amount}
         </span>}
-        <div className="bar-main my-1 rounded-2" style={{height: amount + "%", transition: ".3s"}} onMouseOver={handleHover} onMouseOut={handleHoverOut}></div>
+        
+        <div className={`bar-main my-1 rounded-2 ${maximus}`} style={{height: amount + "%", transition: ".3s"}} onMouseOver={handleHover} onMouseOut={handleHoverOut}></div>
         <p className="day">{day}</p>
     </div>
 }
@@ -18,12 +19,16 @@ function Chart() {
     useGlobalEffect(data);
 
     return <div className="chart d-flex align-items-center justify-content-around">
-        {information.map( (information, informationIndex)=> {
+        {information.map( (information, informationIndex, informationArr)=> {
+            let classToBeAdded = "null";
+            let maximumPrice = Math.max(...informationArr.map(item => item.amount));
+            information.amount === maximumPrice && (classToBeAdded = "max");
             return <Bar
                 key={informationIndex}
                 {...information}
                 handleHoverOut = {() => hidePrice()}
                 handleHover = {()=>showPrice(informationIndex)}
+                maximus={classToBeAdded}
             />
         })}
     </div>
